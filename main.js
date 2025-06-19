567cb9a2b0024c195d1c8f9ed801c7d4c84b97a8
justinrausch <justin@mastery.to>
2025-06-18 23:44:42 -0500

    Basic Project Setup

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Project Imported Successfully</h1>
+    <p>Your project is ready to be customized.</p>
+  </div>
+`
